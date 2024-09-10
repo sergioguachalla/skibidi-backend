08588f21c53d51161ce6d7075231109fe1e913d8
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-09-10 07:43:24 -0400

    first commit

---
+++ src/main/java/com/ucb/skibidi/SkibidiApplication.java
@@ -0,0 +1,13 @@
+package com.ucb.skibidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkibidiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkibidiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucb/skibidi/SkibidiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucb.skibidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkibidiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
