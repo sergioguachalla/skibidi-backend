069e9dabf395fe9087dd6caffb7e2cc987a254ee
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-09-11 17:59:04 -0400

    add controller advice and invalid input exception

---
+++ src/main/java/com/ucb/skibidi/config/ExceptionHandlerController.java
@@ -0,0 +1,14 @@
+package com.ucb.skibidi.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    private static final Logger log = LoggerFactory.getLogger(ExceptionHandlerController.class);
+
+
+}
