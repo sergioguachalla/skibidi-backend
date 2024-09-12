c7c62fe44c2a40018a83ee353f093e8236a3e648
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-09-12 08:19:07 -0400

    add id number validation to validation utils

--- src/main/java/com/ucb/skibidi/utils/ValidationUtils.java
+++ src/main/java/com/ucb/skibidi/utils/ValidationUtils.java
@@ -25,17 +25,27 @@ public class ValidationUtils {
         }
     }
 
+    public static void validateIdNumber(int idNumber) {
+        if (idNumber == 0) {
+            throw new InvalidInputException("Id number is required");
+        }
+        if (!isIdNumberValid(idNumber)) {
+            throw new InvalidInputException("Invalid must be between 5 and 9 digits");
+        }
+    }
+
     private static boolean isNullOrEmpty(String str) {
         return str == null || str.isEmpty();
     }
-
     private static boolean isNameValid(String name) {
         return name.matches("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
     }
-
-
     private static boolean isEmailValid(String email) {
         return email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+    }
+    private static boolean isIdNumberValid(int idNumber) {
+        return String.valueOf(idNumber).length() > 4 && String.valueOf(idNumber).length() < 10
+                && String.valueOf(idNumber).matches("[0-9]+");
     }
 
 
