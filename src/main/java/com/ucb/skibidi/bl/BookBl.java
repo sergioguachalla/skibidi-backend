dbf5ee149c20cd6f91bf9590beaf89b73deb8617
GuidoAlfaro <ardayaalfaro@gmail.com>
2024-09-14 11:20:51 -0400

    Como bibliotecario quiero poder registrar un libro para que esté disponible en la librería y los clientes puedan utilizarlos. Bl

---
+++ src/main/java/com/ucb/skibidi/bl/BookBl.java
@@ -0,0 +1,148 @@
+package com.ucb.skibidi.bl;
+
+import com.ucb.skibidi.config.exceptions.InvalidInputException;
+import com.ucb.skibidi.dao.BookRepository;
+import com.ucb.skibidi.dto.BookDto;
+import com.ucb.skibidi.entity.Book;
+import com.ucb.skibidi.utils.ValidationUtils;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class BookBl {
+
+    // bl de google libros
+    @Autowired
+    GoogleBooksBl googleBooksBl;
+
+    //dao libros
+    @Autowired
+    BookRepository bookRepository;
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(BookBl.class);
+
+    public void createBookManually(BookDto bookDto) throws Exception {
+        log.info("Creating book...");
+        try {
+            validateBook(bookDto);
+            saveBook(bookDto);
+            log.info("Book created {}", bookDto.toString());
+        } catch (Exception e) {
+            log.error("Error creating book: {}", e.getMessage());
+            throw e;
+        }
+
+    }
+
+    public BookDto createBookByISBN(String isbn) throws Exception{
+        log.info("Creating book...");
+        try {
+            validateISBN(isbn);
+            BookDto bookInfo = new BookDto();
+            bookInfo.setIsbn(isbn);
+            bookInfo = getBookInfo(bookInfo);
+            validateBook(bookInfo);
+            saveBook(bookInfo);
+            log.info("Book created {}", bookInfo.toString());
+            return bookInfo;
+        } catch (Exception e) {
+            log.error("Error creating book: {}", e.getMessage());
+            throw e;
+        }
+
+    }
+
+
+    //////////////////
+
+    public void saveBook(BookDto bookDto) throws Exception {
+        log.info("Saving book...");
+        try {
+            //entidad libro
+            Book bookEntity = new Book();
+            bookEntity.setTitle(bookDto.getTitle());
+            bookEntity.setIsbn(bookDto.getIsbn());
+            bookEntity.setRegistrationDate(bookDto.getRegistrationDate());
+            bookEntity.setStatus(bookDto.getStatus());
+            bookRepository.save(bookEntity);
+            log.info("Book saved {}", bookEntity.toString());
+        } catch (Exception e) {
+            log.error("Error saving book: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    public BookDto getBookByISBN(String isbn) throws Exception {
+        log.info("Getting book...");
+        try {
+            validateISBN(isbn);
+            Book bookEntity = new Book();
+            BookDto bookDto = new BookDto();
+            bookEntity = bookRepository.findByIsbn(isbn);
+            if (bookEntity == null) {
+                log.error("Book with ISBN {} not found", isbn);
+                throw new NullPointerException("Book not found with ISBN: " + isbn);
+            }
+            bookDto.setTitle(bookEntity.getTitle());
+            bookDto.setIsbn(bookEntity.getIsbn());
+            bookDto.setRegistrationDate(bookEntity.getRegistrationDate());
+            bookDto.setStatus(bookEntity.getStatus());
+            log.info("Book found {}",bookEntity.toString());
+            return bookDto;
+        } catch (Exception e) {
+            log.error("Error getting book: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    public List<BookDto> getAllBooks() throws Exception {
+        log.info("Getting all books...");
+        try {
+            List<Book> bookEntities = bookRepository.findAll();
+            List<BookDto> booksDto = new ArrayList<>();
+            for (Book bookEntity : bookEntities) {
+                BookDto bookDto = new BookDto();
+                bookDto.setTitle(bookEntity.getTitle());
+                bookDto.setIsbn(bookEntity.getIsbn());
+                bookDto.setRegistrationDate(bookEntity.getRegistrationDate());
+                bookDto.setStatus(bookEntity.getStatus());
+                booksDto.add(bookDto);
+            }
+            log.info("Books found {}",booksDto.toString());
+            return booksDto;
+        } catch (Exception e) {
+            log.error("Error getting books: {}", e.getMessage());
+            throw e;
+        }
+    }
+
+    //////////////////
+
+
+    public void validateISBN(String isbn) throws InvalidInputException {
+        ValidationUtils.validateISBN(isbn);
+    }
+
+    public void validateBook(BookDto bookDto) throws InvalidInputException {
+        ValidationUtils.validateISBN(bookDto.getIsbn());
+        ValidationUtils.validateTitle(bookDto.getTitle());
+        ValidationUtils.validateRegistrationDate(bookDto.getRegistrationDate());
+        ValidationUtils.validateStatus(bookDto.getStatus());
+    }
+
+    public BookDto getBookInfo(BookDto bookDto) {
+        log.info("Getting book info...");
+        try {
+            return googleBooksBl.getBookByIsbn(bookDto.getIsbn());
+
+        } catch (Exception e) {
+            log.error("Error getting book info: {}", e.getMessage());
+            return bookDto;
+        }
+    }
+}
