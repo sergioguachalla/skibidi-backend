7afb3971f01f128ff4b73ab1d94e463790c2d019
Alejandro <alejandro.flores.a@ucb.edu.bo>
2024-09-19 18:07:35 -0400

    genre api added

---
+++ src/main/java/com/ucb/skibidi/api/GenreApi.java
@@ -0,0 +1,10 @@
+package com.ucb.skibidi.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/genre")
+public class GenreApi {
+
+}
