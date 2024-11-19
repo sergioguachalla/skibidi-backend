59834627a9967b8083fac769200fe26dbde2deae
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-11-19 11:41:13 -0400

    add type fine bl

---
+++ src/main/java/com/ucb/skibidi/bl/TypeFineBl.java
@@ -0,0 +1,30 @@
+package com.ucb.skibidi.bl;
+
+import com.ucb.skibidi.dao.TypeFineRepository;
+import com.ucb.skibidi.dto.TypeFineDto;
+import com.ucb.skibidi.entity.TypeFines;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TypeFineBl {
+
+    @Autowired
+    private TypeFineRepository typeFineRepository;
+
+    public List<TypeFineDto> findAll(){
+        List<TypeFines> typeFineList = typeFineRepository.findAll();
+        List<TypeFineDto> typeFineDtoList = new ArrayList<>();
+        for(TypeFines typeFine : typeFineList){
+            TypeFineDto typeFineDto = new TypeFineDto();
+            typeFineDto.setId(typeFine.getTypeFineId());
+            typeFineDto.setDescription(typeFine.getDescription());
+            typeFineDto.setAmount(typeFine.getAmount());
+            typeFineDtoList.add(typeFineDto);
+        }
+        return typeFineDtoList;
+    }
+}
