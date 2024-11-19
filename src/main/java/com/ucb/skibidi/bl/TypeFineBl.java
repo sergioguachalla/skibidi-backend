5250e6d7d05f59ab16a3e7e9e002b86fa65378be
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-11-19 11:41:13 -0400

    add type fine bl

--- src/main/java/com/ucb/skibidi/bl/TypeFineBl.java
+++ src/main/java/com/ucb/skibidi/bl/TypeFineBl.java
@@ -1,8 +1,5 @@
 package com.ucb.skibidi.bl;
 
-import com.ucb.skibidi.config.exceptions.TypeFineDeleteException;
-import com.ucb.skibidi.dao.FineRepository;
-import com.ucb.skibidi.dao.LendBookRepository;
 import com.ucb.skibidi.dao.TypeFineRepository;
 import com.ucb.skibidi.dto.TypeFineDto;
 import com.ucb.skibidi.entity.TypeFines;
@@ -17,8 +14,6 @@ public class TypeFineBl {
 
     @Autowired
     private TypeFineRepository typeFineRepository;
-    @Autowired
-    FineRepository fineRepository;
 
     public List<TypeFineDto> findAll(){
         List<TypeFines> typeFineList = typeFineRepository.findAll();
@@ -32,30 +27,4 @@ public class TypeFineBl {
         }
         return typeFineDtoList;
     }
-
-    public void saveTypeFine(TypeFineDto typeFineDto){
-        TypeFines typeFine = new TypeFines();
-        typeFine.setDescription(typeFineDto.getDescription());
-        typeFine.setAmount(typeFineDto.getAmount());
-        typeFineRepository.save(typeFine);
-    }
-
-    public void updateTypeFine(TypeFineDto typeFineDto){
-        var typeFine = typeFineRepository.findById(typeFineDto.getId());
-        if(typeFine.isPresent()){
-            TypeFines typeFineToUpdate = typeFine.get();
-            typeFineToUpdate.setDescription(typeFineDto.getDescription());
-            typeFineToUpdate.setAmount(typeFineDto.getAmount());
-            typeFineRepository.save(typeFineToUpdate);
-        }
-    }
-
-    public void deleteTypeFine(Long id){
-        if(fineRepository.existsByTypeFineTypeFineId(id)){
-            throw new TypeFineDeleteException
-                    ("No se puede eliminar el tipo de multa porque hay multas asociadas a este tipo");
-        }
-        typeFineRepository.deleteById(id);
-    }
-
 }
