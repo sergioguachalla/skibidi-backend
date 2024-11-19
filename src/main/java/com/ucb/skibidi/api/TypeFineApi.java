cec08d7cdd48d8e97a01954778a0c6a114630e85
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-11-19 11:50:26 -0400

    add rest controllers for fine type

---
+++ src/main/java/com/ucb/skibidi/api/TypeFineApi.java
@@ -0,0 +1,48 @@
+package com.ucb.skibidi.api;
+
+import com.ucb.skibidi.bl.TypeFineBl;
+import com.ucb.skibidi.dto.ResponseDto;
+import com.ucb.skibidi.dto.TypeFineDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/typeFines")
+public class TypeFineApi {
+
+    @Autowired
+    private TypeFineBl typeFineBl;
+
+
+    @GetMapping("/")
+    public ResponseDto<List<TypeFineDto>> findAll(){
+        List<TypeFineDto> typeFineDtoList = typeFineBl.findAll();
+        ResponseDto<List<TypeFineDto>> responseDto = new ResponseDto<>();
+        responseDto.setSuccessful(true);
+        responseDto.setMessage("OK");
+        responseDto.setData(typeFineDtoList);
+        return responseDto;
+    }
+
+    @PutMapping("/")
+    public ResponseDto<TypeFineDto> updateFineDto(@RequestBody TypeFineDto typeFineDto){
+        typeFineBl.updateTypeFine(typeFineDto);
+        ResponseDto<TypeFineDto> responseDto = new ResponseDto<>();
+        responseDto.setSuccessful(true);
+        responseDto.setMessage("fine updated successfully");
+        responseDto.setData(typeFineDto);
+        return responseDto;
+    }
+
+    @PostMapping("/")
+    public ResponseDto<TypeFineDto> saveFineDto(@RequestBody TypeFineDto typeFineDto){
+        typeFineBl.saveTypeFine(typeFineDto);
+        ResponseDto<TypeFineDto> responseDto = new ResponseDto<>();
+        responseDto.setSuccessful(true);
+        responseDto.setMessage("fine saved successfully");
+        responseDto.setData(typeFineDto);
+        return responseDto;
+    }
+}
